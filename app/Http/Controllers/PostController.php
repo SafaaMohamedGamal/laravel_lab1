badbec5807a223784bff711592fbe86de17f4246
SafaaMohamedGamal <safaa.mohamed.gamal@gmail.com>
2020-03-26 18:13:04 +0200

    initializing intake-40 project and create posts table

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Post;
+class PostController extends Controller
+{
+    public function index()
+    {
+        // $posts = Post::all();
+        $posts=[
+            [
+                'id' => '22',
+                'title' => 'first post',
+                'created_at' => '2020-05-02',
+            ],
+            [
+                'id' => '24',
+                'title' => 'second post',
+                'created_at' => '2018-05-02',
+            ],
+            [
+                'id' => '26',
+                'title' => 'third post',
+                'created_at' => '2019-05-02',
+            ]
+            ];
+        return view('index',[
+            "posts" => $posts
+        ]);
+    }
+
+    public function show()
+    {
+        $request = request();
+        $postId = $request->post;
+
+        $post = Post::find($postId);
+        dd($post);
+        return view('show', [
+            "post" => $post
+        ]);
+    }
+}
